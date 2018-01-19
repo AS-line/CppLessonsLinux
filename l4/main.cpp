c7f9507ba9550e17d757c216ef49a64d17bb5b63
AS-line <taibov.0321@mail.ru>
2018-01-19 22:18:53 +0300

    index this

---
+++ l4/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+// changes for git checkout
+class Test {
+private:
+    int a, b, c;
+public:
+    void set(int a, int b, int c) {
+        this->a = a; //вместо a = a_a;
+        this->b = b; //вместо b = b_b;
+        this->c = c; //вместо c = c_c;
+    }
+    void get() {
+        cout << "A=" << a << "  B=" << b << "  C=" << c << endl;
+    }
+};
+
+int main()
+{
+    Test obj;
+    obj.set(12, 13, 14);
+    obj.get();
+    return 0;
+}
